04e518b0044cb002075183b52d814a16ba49a61d
Sai Sathwika Puri <saisathwikap1981@gmail.com>
2026-05-21 13:29:33 +0530

    Initial Commit

---
+++ MatchMate/MatchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by Sai Sathwika Puri on 21/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
